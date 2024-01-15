b28658dc0617eab6015007e4b23c67a412b6869b
erick-rios <erickrg2700@ciencias.unam.mx>
2024-01-15 16:20:01 -0600

    version 0.1.1

--- build/lib.linux-x86_64-cpython-39/isingenerator/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-A Python Package to easily generate data of the Ising Model using the Metropolis Algorithm, 
-the most commonly used Markov Chain Monte Carlo method to calculate estimations for this system.
-"""
